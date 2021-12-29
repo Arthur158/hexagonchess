88d966539404848a6d0212ff6c0b28089b8224bc
Arthur <arthur.jacques.be@gmail.com>
2021-12-29 18:28:28 +0100

    startr of the implementation of the checker.

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -44,4 +44,39 @@ function PositionChecker() {
  */
 PositionChecker.prototype.checkPosition = function(p1, p2, board, pieceType, color) {
     // board.getPieceAtPosition(Position) -> {PieceType, color} | null
+    let pieceStartingPosition=board.getPieceAtPosition(p1);
+    let pieceEndingPosition=board.getPieceAtPosition(p2);
+
+    let vector=new Position(p2.x-p1.x,p2.y-p1.y,p2.z-p1.z);
+
+    while(vector.x > 0 && vector.y < 0){
+        vector.x--;
+        vector.y++;
+        vector.z++;
+    }
+    while(vector.x > 0 && vector.z < 0){
+        vector.x--;
+        vector.z++;
+        vector.y++;
+    }
+    while(vector.y > 0 && vector.z > 0){
+        vector.z--;
+        vector.y--;
+        vector.x++;
+    } 
+
+    if(vector.x==0 && vector.y==0 &&vector.z==0){
+        return false;
+    }
+
+    switch(pieceStartingPOsition[0]){
+        case PieceType.KING:
+            if(Math.abs(vector.x)<2 && Math.abs(vector.y)<2 && Math.abs(vector.z)<2){
+                if(){
+                    
+                }
+            } 
+        case  PieceType.KNIGHT:
+
+    }
 }
