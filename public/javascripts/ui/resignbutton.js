5cb45940e7947d9cba49ac89b7607d2078551b87
Arthur <arthur.jacques.be@gmail.com>
2022-01-14 14:38:45 +0100

    almost working resign interface

--- public/javascripts/ui/resignbutton.js
+++ public/javascripts/ui/resignbutton.js
@@ -2,14 +2,29 @@ class ResignButton{
     constructor(){
         this.resignButton = document.getElementById("forfeitBtn");
         this.gameHandler=null;
+        this.resignQuestion=document.getElementById("forfeitAsk");
+        this.resignYes=document.getElementById("forfeitYes");
+        this.resignNo=document.getElementById("forfeitNo");
     }
     initialize(gameHandler) {
         this.initializeResignButton(gameHandler);
+
     }
 
     initializeResignButton(gameHandler) {
         this.resignButton.addEventListener("click",function singleClick(e){
+            document.getElementById("forfeitAsk").style.display="inline";
+            document.getElementById("forfeitYes").style.display="inline";
+            document.getElementById("forfeitNo").style.display="inline";
+        })    
+
+        this.resignYes.addEventListener("click",function singleClick(e){
             gameHandler.resignMessage();
+        })
+        this.resignNo.addEventListener("click",function singleClick(e){
+            document.getElementById("forfeitAsk").style.display="none";
+            document.getElementById("forfeitYes").style.display="none";
+            document.getElementById("forfeitNo").style.display="none";
         })
     }
 
