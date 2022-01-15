5819340964acd438c9e1475dde7c5b53c114db57
Arthur <arthur.jacques.be@gmail.com>
2022-01-15 17:03:29 +0100

    some css stuff

--- public/javascripts/ui/resignbutton.js
+++ public/javascripts/ui/resignbutton.js
@@ -2,7 +2,6 @@ class ResignButton{
     constructor(){
         this.resignButton = document.getElementById("forfeitBtn");
         this.gameHandler=null;
-        this.resignQuestion=document.getElementById("forfeitAsk");
         this.resignYes=document.getElementById("forfeitYes");
         this.resignNo=document.getElementById("forfeitNo");
     }
@@ -13,18 +12,14 @@ class ResignButton{
 
     initializeResignButton(gameHandler) {
         this.resignButton.addEventListener("click",function singleClick(e){
-            document.getElementById("forfeitAsk").style.display="inline";
-            document.getElementById("forfeitYes").style.display="inline";
-            document.getElementById("forfeitNo").style.display="inline";
+            document.getElementById("forfeitSquare").style.display="inline";
         })    
 
         this.resignYes.addEventListener("click",function singleClick(e){
             gameHandler.resignMessage();
         })
         this.resignNo.addEventListener("click",function singleClick(e){
-            document.getElementById("forfeitAsk").style.display="none";
-            document.getElementById("forfeitYes").style.display="none";
-            document.getElementById("forfeitNo").style.display="none";
+            document.getElementById("forfeitSquare").style.display="none";
         })
     }
 
